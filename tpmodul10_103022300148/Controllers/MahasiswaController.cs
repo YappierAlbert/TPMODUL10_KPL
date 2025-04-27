1d78c6420c32d735bee0e891e2b116d7b8901be7
Yappier Albertus <yappier.albert1@gmail.com>
2025-04-27 17:57:33 +0700

    Menambahkan API Mahasiswa

---
+++ tpmodul10_103022300148/Controllers/MahasiswaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul10_103022300148.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static readonly List<Mahasiswa> mahasiswa = new()
+        {
+            new Mahasiswa ( "Yappier Albertus Febriandi Krisna Putra", 103022300148 ),
+            new Mahasiswa ( "Adrian Putra Perkasa", 103022300041 ),
+            new Mahasiswa ( "Muhammad Arrayan Fikri", 103022300050 ),
+            new Mahasiswa ( "Achmad Maulana Arief", 103022330157 ),
+        };
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswa;
+        }
+        [HttpGet("{id}")]
+        public Mahasiswa GetId(int id)
+        {
+            return mahasiswa[id];
+        }
+        [HttpDelete("{id}")]
+        public String Delete(int id) {
+            mahasiswa.RemoveAt(id);
+            return "Data berhasil dihapus";
+        }
+        [HttpPost]
+        public String Post([FromBody] Mahasiswa mhs){
+            mahasiswa.Add(mhs);
+            return "Data berhasil ditambah";
+        }
+    }
+}
---
+++ tpmodul10_103022300148/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace tpmodul10_103022300148
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public long nim { get; set; }
+        public Mahasiswa() { }
+        public Mahasiswa(string nama,long nim)
+        {
+            this.nama = nama;
+            this.nim = nim;
+        }
+    }
+}
